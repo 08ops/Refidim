b69358cd21c2be05bffb9f81536e54bc4842465f
08ops <danillo3412@gmail.com>
2025-10-06 11:46:58 +0000

    done with temp

---
+++ Python_Track/Temperature_Conversion.py
@@ -0,0 +1,4 @@
+deg_celcius = float(input("Enter temperature in Celsius: "))
+deg_fahrenheit = (deg_celcius * 1.80) + 32
+deg_kelvin = deg_celcius + 273.15
+print(f"[{deg_kelvin}, {deg_fahrenheit}]")
